6ea2bc9c29597b404b174caaa63aef61d7b2a967
felipessalvatore <felipessalvador@egooglemail.com>
2017-02-28 21:48:21 -0300

    3e ready

--- source/q3_RNNLM.py
+++ source/q3_RNNLM.py
@@ -29,7 +29,7 @@ class Config(object):
     def __init__(self,
                  embed_size=50,
                  batch_size=104,
-                 num_steps=10,
+                 num_steps=14,
                  hidden_size=100,
                  max_epochs=16,
                  early_stopping=2,
