a73491b298f0cdacde8616c78823d116baafa633
Felipe Salvatore <felsal@logprob.ime.usp.br>
2017-03-02 10:48:16 -0300

    Second modulo import test

--- source/search/NER_batch_search.py
+++ source/search/NER_batch_search.py
@@ -1,4 +1,11 @@
-from ..q2_NER import test_NER, Config
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from q2_NER import test_NER, Config
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
